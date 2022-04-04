2c1c0a1c3d30ac7323a5ed72a906f62256fe835f
mnin-nahid <nahidulislam.nayan54@gmail.com>
2022-04-04 22:17:07 +0600

    setup done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Router, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-6xl font-bold">
+        Hello world!
+      </h1>
+      <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/home' element={<Home></Home>}></Route>
+          <Route path='/about' element={<About></About>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h3 className='text-3xl mt-8'>We are the leader of the web.</h3>
+        </div>
+    );
+};
+
+export default About;
